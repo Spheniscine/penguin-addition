ebf2b11fbe56531fe89c02141b23b4e5a9045063
Spheniscine <spheniscine@gmail.com>
2025-06-05 11:07:34 +0800

    random and math rendering test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use dioxus::prelude::*;
+use components::Math;
+
+mod components;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -12,6 +15,13 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Link {
+            rel: "stylesheet",
+            href: "https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css",
+            integrity: "sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib",
+            crossorigin: "anonymous"
+        }
+        
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS } 
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
@@ -23,11 +33,13 @@ fn App() -> Element {
 
 #[component]
 pub fn Hero() -> Element {
+    use rand::Rng;
     rsx! {
         div {
             id: "hero",
             img { src: HEADER_SVG, id: "header" }
             div { id: "links",
+                Math { tex: {rand::rng().random_range(1..=100)} }
                 a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
                 a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
                 a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
