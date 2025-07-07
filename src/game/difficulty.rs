d5968574a06ca52ddd996553a72617bfc75e129f
Spheniscine <spheniscine@gmail.com>
2025-07-07 19:42:51 +0800

    translate difficulty to string map and back

--- src/game/difficulty.rs
+++ src/game/difficulty.rs
@@ -1,3 +1,4 @@
+use indexmap::{IndexMap, indexmap};
 use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumIter, EnumString};
 
@@ -16,10 +17,34 @@ pub struct Difficulty {
 
 impl Difficulty {
     pub const RESULT_MAXES: &[i32] = &[10, 20, 50, 100];
+    pub const STR_OPERATOR: &str = "operator";
+    pub const STR_MAX_RESULT: &str = "max_result";
+    pub const STR_ADDEND_RANGE: &str = "addend_range";
+}
+
+impl Difficulty {
+    pub fn to_map(&self) -> IndexMap<&str, String> {
+        indexmap! {
+            Self::STR_OPERATOR => self.operator.to_string(),
+            Self::STR_MAX_RESULT => self.max_result.to_string(),
+            Self::STR_ADDEND_RANGE => format!("{},{}", self.min_addend, self.max_addend),
+        }
+    }
+
+    pub fn from_map(map: &IndexMap<&str, String>) -> Option<Self> {
+        let max_result = map.get(Self::STR_MAX_RESULT)?.parse::<i32>().ok()?;
+        let mut addends = map.get(Self::STR_ADDEND_RANGE)?.split(',');
+        let min_addend = addends.next()?.parse::<i32>().ok()?;
+        let max_addend = addends.next()?.parse::<i32>().ok()?;
+        let operator = map.get(Self::STR_OPERATOR)?.parse::<Operator>().ok()?;
+
+        Some(Self {
+            max_result, min_addend, max_addend, operator
+        })
+    }
 }
 
 impl Default for Difficulty {
-
     fn default() -> Self {
         Self { max_result: 10, min_addend: 1, max_addend: 1, operator: Operator::Plus }
     }
